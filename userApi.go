ffacea92a149097a9395f2327dd5b283636318e5
lahari M <73182046+lahariiii@users.noreply.github.com>
2021-10-09 17:15:50 +0530

    Add files via upload

---
+++ userApi.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"dao"
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"gopkg.in/mgo.v2/bson"
+)
+
+// CREATE USER
+func CreateUserEndPoint(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	var user User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	user.Id = bson.NewObjectId()
+	if err := dao.Insert(user); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusCreated, user)
+}
+
+// GET ALL USERS
+func AllUsersEndPoint(w http.ResponseWriter, r *http.Request) {
+	users,
+		err := dao.FindAll()
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusOK, users)
+}
+
+// GET USER BY ID
+func FindUserEndpoint(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	user,
+		err := dao.FindById(params["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid User ID")
+		return
+	}
+	respondWithJson(w, http.StatusOK, user)
+}
